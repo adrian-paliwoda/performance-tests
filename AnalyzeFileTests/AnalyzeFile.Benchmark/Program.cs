2ec58b905c2afe908a592ad88cb05aee4e477e19
Adrian Paliwoda <adrian94@tutanota.com>
2025-12-25 17:08:06 +0100

    Added reference to benchmark

--- AnalyzeFileTests/AnalyzeFile.Benchmark/Program.cs
+++ AnalyzeFileTests/AnalyzeFile.Benchmark/Program.cs
@@ -1,8 +1,9 @@
 ﻿using AnalyzeFile.Benchmark.Benchmarks;
 using BenchmarkDotNet.Running;
 
-// var summary = BenchmarkRunner.Run<AnalyzeFile.Benchmark.Benchmarks.AnalyzerFile>();
-var summary = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+// var summary = BenchmarkRunner.Run<AnalyzeFile.Benchmark.Benchmarks.AnalyzerSmallFile>();
+var summary = BenchmarkRunner.Run<AnalyzeManyFiles.Benchmark.Benchmarks.AnalyzerMediumFiles>();
+// var summary = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
 // var test = new AnalyzerFile();
-// test.SmallFile_StreamReaderWithReadLineStrategy();
+// test.SmallFile_StreamReaderWithReadLineStrateg1y();
